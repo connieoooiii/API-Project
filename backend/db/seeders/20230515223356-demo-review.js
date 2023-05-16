2cebdc12af8009299850c7c14f78dd30d8020f5e
Connie Ooi <connie.ooi18@gmail.com>
2023-05-15 21:06:19 -0700

    finish seeders

--- backend/db/seeders/20230515223356-demo-review.js
+++ backend/db/seeders/20230515223356-demo-review.js
@@ -1,59 +1,49 @@
 "use strict";
-const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
+const reviews = [
+  {
+    spotId: 1,
+    userId: 2,
+    review:
+      "I had an absolutely wonderful stay at the Pacifica Beach Cottage. The cozy atmosphere and stunning views made it the perfect retreat. ",
+    stars: 5,
+  },
+  {
+    spotId: 2,
+    userId: 3,
+    review:
+      "The Seattle condo was the perfect choice for my stay. The modern and stylish design combined with the unbeatable location next to the Space Needle made it a truly memorable experience. The breathtaking views of the city skyline added an extra touch of luxury to my stay.",
+    stars: 5,
+  },
+  {
+    spotId: 3,
+    userId: 4,
+    review:
+      "While the Manhattan condo had a great location and vibrant neighborhood, the overall experience fell slightly short of expectations. The beautiful brownstone architecture added charm, but the condo lacked some essential amenities and the price seemed a bit high. Nonetheless, it served as a decent base for exploring the city",
+    stars: 3.5,
+  },
+  {
+    spotId: 4,
+    userId: 1,
+    review:
+      "I had a fantastic stay at the Honolulu apartment. The panoramic oceanview from the apartment was absolutely breathtaking and the proximity to the beach was a major highlight. The only reason I couldn't give it a full 5 stars was some minor maintenance issues.",
+    stars: 4,
+  },
+];
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = "Reviews";
-    return queryInterface.bulkInsert(
-      options,
-      [
-        {
-          spotId: 1,
-          userId: 2,
-          review:
-            "I had an absolutely wonderful stay at the Pacifica Beach Cottage. The cozy atmosphere and stunning views made it the perfect retreat. ",
-          stars: 5,
-        },
-        {
-          spotId: 2,
-          userId: 3,
-          review:
-            "The Seattle condo was the perfect choice for my stay. The modern and stylish design combined with the unbeatable location next to the Space Needle made it a truly memorable experience. The breathtaking views of the city skyline added an extra touch of luxury to my stay.",
-          stars: 5,
-        },
-        {
-          spotId: 3,
-          userId: 4,
-          review:
-            "While the Manhattan condo had a great location and vibrant neighborhood, the overall experience fell slightly short of expectations. The beautiful brownstone architecture added charm, but the condo lacked some essential amenities and the price seemed a bit high. Nonetheless, it served as a decent base for exploring the city",
-          stars: 3.5,
-        },
-        {
-          spotId: 4,
-          userId: 1,
-          review:
-            "I had a fantastic stay at the Honolulu apartment. The panoramic oceanview from the apartment was absolutely breathtaking and the proximity to the beach was a major highlight. The only reason I couldn't give it a full 5 stars was some minor maintenance issues.",
-          stars: 4,
-        },
-      ],
-      {}
-    );
+    return queryInterface.bulkInsert(options, reviews, {});
   },
 
   down: async (queryInterface, Sequelize) => {
     options.tableName = "Reviews";
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      options,
-      {
-        username: {[Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"]},
-      },
-      {}
-    );
+    return queryInterface.bulkDelete(options.tableName, {}, {});
   },
 };
--- backend/db/seeders/20230515223454-demo-review-image.js
+++ backend/db/seeders/20230515223454-demo-review-image.js
@@ -5,40 +5,33 @@ if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
+const reviewImgs = [
+  {
+    reviewId: 1,
+    url: "https://www.example.com/images/random-pic4.jpg",
+  },
+  {
+    reviewId: 2,
+    url: "https://www.example.com/images/random-image5.png",
+  },
+  {
+    reviewId: 3,
+    url: "https://www.example.com/images/random-photo6.jpeg",
+  },
+  {
+    reviewId: 4,
+    url: "https://www.example.com/images/random-pic7.jpg",
+  },
+];
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = "ReviewImages";
-    return queryInterface.bulkInsert(
-      options,
-      [
-        {
-          reviewId: 1,
-          url: "https://www.example.com/images/random-pic4.jpg",
-        },
-        {
-          reviewId: 2,
-          url: "https://www.example.com/images/random-image5.png",
-        },
-        {
-          reviewId: 3,
-          url: "https://www.example.com/images/random-photo6.jpeg",
-        },
-        {
-          reviewId: 4,
-          url: "https://www.example.com/images/random-pic7.jpg",
-        },
-      ],
-      {}
-    );
+    return queryInterface.bulkInsert(options, reviewImgs, {});
   },
 
   down: async (queryInterface, Sequelize) => {
     options.tableName = "ReviewImages";
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      options,
-      {address: {[Op.in]: addressToDelete}},
-      {}
-    );
+    return queryInterface.bulkDelete(options.tableName, {}, {});
   },
 };
