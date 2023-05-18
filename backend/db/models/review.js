cb717c5db51fda0f9f7008b97d0d9871f18dc291
Connie Ooi <connie.ooi18@gmail.com>
2023-05-17 22:21:35 -0700

    added delete an existing reveiw

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -16,6 +16,8 @@ module.exports = (sequelize, DataTypes) => {
       });
       Review.hasMany(models.ReviewImage, {
         foreignKey: "reviewId",
+        onDelete: "CASCADE",
+        hooks: true,
       });
     }
   }
